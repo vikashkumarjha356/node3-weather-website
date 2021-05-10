8a9ccb4d646b0109ae48ae07ee24afaeb680b4e1
Vikash Jha <103767@bk.local>
2021-05-10 12:10:56 +0530

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const foreCast = (lat, long , callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=536d15d9871925266e22037adf7be0b5&query=${lat},${long}&unit=m`;
+    request({ url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect',undefined)
+        }
+
+        else if(body.error){
+            callback('Unable to find c0-ordinates',undefined)
+        }
+
+        else{
+            callback(undefined, body.current.weather_descriptions[0]
+                + ". It is currently " + body.current.temperature +" degrees out")
+        }
+     })
+}
+
+
+
+module.exports = foreCast
